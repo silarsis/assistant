886220c4a4bd546417b7afe1d5d97090e455a522
Kevin Littlejohn <kevin@littlejohn.id.au>
2023-05-16 23:10:34 +0800

    first draft whatsapp message sender

--- agent/app.py
+++ agent/app.py
@@ -1,4 +1,5 @@
 import asyncio
+import aiohttp
 import os
 import ssl
 from websockets.server import serve
@@ -57,6 +58,7 @@ class API:
                 except json.JSONDecodeError as e:
                     print(str(e))
                     continue
+                # Check if prompt from web or message from whatsapp
                 if (m.get("type") == 'prompt'):
                     correlation_id=f'{uuid.uuid4()}'
                     def callback(x):
@@ -75,6 +77,40 @@ class API:
         except ConnectionClosedError:
             print("WS connection broken")
             await websocket.close()
+
+    def format_whatsapp_outbound(self, recipient: str, text: str) -> str:
+        return json.dumps({
+            "messaging_product": "whatsapp",
+            "preview_url": False,
+            "recipient_type": "individual",
+            "to": recipient,
+            "type": "text",
+            "text": {
+                "body": text
+            }
+        })
+
+    async def send_whatsapp_message(self, text: str):
+        # Send a message to whatsapp
+        headers = {
+            "Content-type": "application/json",
+            "Authorization": f"Bearer {os.environ.get('WHATSAPP_ACCESS_TOKEN')}",
+        }
+        data = self.format_whatsapp_outbound(os.environ.get('WHATSAPP_RECIPIENT_WAID'), text)
+        async with aiohttp.ClientSession() as session:
+            url = 'https://graph.facebook.com/v13.0/' + f"{os.environ.get('WHATSAPP_PHONE_NUMBER_ID')}/messages"
+            try:
+                async with session.post(url, data=data, headers=headers) as response:
+                    if response.status == 200:
+                        print("Status:", response.status)
+                        print("Content-type:", response.headers['content-type'])
+                        html = await response.text()
+                        print("Body:", html)
+                    else:
+                        print(response.status)        
+                        print(response)        
+            except aiohttp.ClientConnectorError as e:
+                print('Connection Error', str(e))
 
     async def main(self):
         print("Launching WS server")
