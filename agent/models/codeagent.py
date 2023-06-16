f54b15d4d4ede1c221066abb54ebd9eaaf987e7a
Kevin Littlejohn <kevin.littlejohn@grabtaxi.com>
2023-06-16 10:29:41 +0800

    new codeagent for code queries

--- agent/models/codeagent.py
+++ agent/models/codeagent.py
@@ -12,7 +12,7 @@ class StarCoderAgent:
         return self._agent.run(query, temperature=0.7, remote=True)
     
 class AzureCodeAgentExplain:
-    query_template = "Please explain the following code:\n{{query}}"
+    query_template = "Please explain the following code, with particular attention to any possible security issues:\n{{query}}"
     def __init__(self):
         self.guide = guidance.llms.OpenAI(
             'code-davinci-002',
