9e7b0b2ea7c4d5e94488efe85c275ee5cb578051
Kevin Littlejohn <kevin@littlejohn.id.au>
2024-03-12 10:30:31 +0800

    fallback for search to scraping the website - untested

--- agent/models/plugins/GoogleSearch.py
+++ agent/models/plugins/GoogleSearch.py
@@ -1,21 +1,28 @@
 from pydantic import BaseModel
 from semantic_kernel.functions.kernel_function_decorator import kernel_function
 
-from langchain_community.utilities import GoogleSearchAPIWrapper
+from googleapiclient.discovery import build
+from search_engine_parser.core.engines.google import Search as GoogleSearch
 
 from config import settings
+
+MAX_RESULTS = 10
 
 class GoogleSearchPlugin(BaseModel):
     @kernel_function(description="Use google search API to return answers for a query, takes a query and returns the scraped text from relevant websites", name="search")
     async def search(self, query: str) -> str:
         """Use the Google Search API to search for and return results for the given query"""
         if settings.google_api_key:
-            search = GoogleSearchAPIWrapper()
-            results = search.results(query, 10)
-        else:
-            results = "No Google API key found. Please set the GOOGLE_API_KEY environment variable."
-        self._convert_results_to_response(results)
-        return str(results)
+            service = build("customsearch", "v1", developerKey=settings.google_api_key)
+            cse = service.cse()
+            results = cse.list(q=query, cx=settings.google_cse_id, num=MAX_RESULTS).execute().get("items", [])
+            formatted_results = '\n'.join([item['snippet'] for item in results]) + '\n\nREFERENCES:\n' + '\n'.join([item['link'] for item in results])
+        else: # Fallback to scraping the search results
+            gsearch = GoogleSearch()
+            resObj = await gsearch.async_search(query, MAX_RESULTS)
+            results = resObj.results
+            formatted_results = '\n'.join([f"{item['titles']} - {item['descriptions']}" for item in results]) + '\n\nREFERENCES:\n' + '\n'.join([item['links'] for item in results])
+        return formatted_results
     
     def _convert_results_to_response(self, results: dict) -> str:
         return '\n'.join([item['snippet'] for item in results]) + '\n\nREFERENCES:\n' + '\n'.join([item['link'] for item in results])
